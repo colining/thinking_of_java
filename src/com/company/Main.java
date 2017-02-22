fdd70f8f3233387dadade89826cc5853bf344798
colining <18810782954@163.com>
2017-02-22 15:38:26 +0800

    l

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,7 @@ package com.company;
 import net.mindview.util.Print;
 
 import java.util.Date;
+import java.util.Random;
 
 import static net.mindview.util.Print.print;
 
@@ -11,13 +12,13 @@ import static net.mindview.util.Print.print;
 public class Main {
 
     public static void main(String[] args) {
-        print("aaa");
-        String s;
-        System.out.println(new Date());
-
-        System.getProperties().list(System.out);
-        System.out.println(System.getProperty("user.name"));
-        System.out.println(System.getProperty("java.library.path"));
+//        print("aaa");
+//        String s;
+//        System.out.println(new Date());
+//
+//        System.getProperties().list(System.out);
+//        System.out.println(System.getProperty("user.name"));
+//        System.out.println(System.getProperty("java.library.path"));
 //        int a =5;
 //        Mouse mouse = new Mouse("aaa",123);
 //        mouse.Mouse1();
@@ -25,14 +26,38 @@ public class Main {
 //        Test test = new Test("lll",2333);
 //        test.Mouse1();
         //int []array1= new int[5];
-        String []s1 = new String[]{"5"};
-        System.out.println(s1.length);
+//        String []s1 = new String[]{"5"};
+//        System.out.println(s1.length);
         //int a= array1.length;
         //System.out.println(array1[5]);
        // Mouse mouse = new Mouse();
 
         //关于赋值引用
-        
+//        Tank tank1 = new Tank();
+//        tank1 .level = 1;
+//        Tank tank2 = new Tank();
+//        tank2. level = 2;
+//        System.out.println(tank1.level);
+//        System.out.println(tank2.level);
+//        tank1 = tank2;
+//        System.out.println(tank1.level);
+//        System.out.println(tank2.level);
+//        tank1.level = 27;
+//
+//        System.out.println(tank2.level);
+
+        //Random
+//        Random random = new Random(47);
+//        int a =random.nextInt(100);
+//        System.out.println(a);
+        //
+//        long a = 200;
+//        System.out.println(Long.toBinaryString(a));
+//        int v= (int) 5e2;
+//        System.out.println(v);
+        //三元运算符
+        int i = 1<2 ? 1:2;
+
     }
 }
 
